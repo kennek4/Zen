7190b9294acda6b21e2b425847dda6d77d92bcb8
kennek4 <kentabanay@gmail.com>
2025-06-28 14:23:35 -0600

    refactor: Updated header names

--- engine/zen.h
+++ engine/zen.h
@@ -4,9 +4,11 @@
 // Base
 #include "base/ZEN_Engine.h"
 #include "base/ZEN_Types.h"
-#include "base/ZEN_Window.h"
+#include "base/ZEN_WindowManager.h"
 
 // Render
-#include "render/ZEN_Render.h"
+#include "renderer/ZEN_GraphicsAPI.h"
+#include "renderer/ZEN_OpenGL.h"
+#include "renderer/ZEN_Renderer.h"
 
 #endif // !ZEN_H
