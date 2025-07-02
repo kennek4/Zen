e3ca4741160a074e631ec9adb8f0750fed921db7
kennek4 <kentabanay@gmail.com>
2025-07-01 21:47:59 -0600

    feat: Moved constructor and deconstructor implm. into .cpp file. Also renamed files to ZEN_Renderer2D.*

---
+++ engine/render/ZEN_Renderer2D.cpp
@@ -0,0 +1,9 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_video.h>
+#include <render/ZEN_Renderer2D.h>
+
+inline ZEN_2D::ZEN_2D() {};
+
+inline ZEN_2D::~ZEN_2D() {};
+
+void ZEN_2D::render() {};
---
+++ engine/render/ZEN_Renderer2D.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <SDL3/SDL_video.h>
+
+class ZEN_2D {
+public:
+  ZEN_2D();
+  ~ZEN_2D();
+
+  void render();
+
+private:
+};
