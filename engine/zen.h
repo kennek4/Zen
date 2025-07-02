dbe4a8759b14273a35354e49c8a92b73032e3acb
kennek4 <kentabanay@gmail.com>
2025-07-01 23:55:47 -0600

    chore: Updated #include statements with updated file names

--- engine/zen.h
+++ engine/zen.h
@@ -4,11 +4,11 @@
 // Base
 #include "base/ZEN_Engine.h"
 #include "base/ZEN_Types.h"
-#include "base/ZEN_WindowManager.h"
+#include "base/managers/ZEN_GameManager.h"
+#include "base/managers/ZEN_ResourceManager.h"
+#include "base/managers/ZEN_WindowManager.h"
 
 // Render
-#include "renderer/ZEN_GraphicsAPI.h"
-#include "renderer/ZEN_OpenGL.h"
-#include "renderer/ZEN_Renderer.h"
+#include "render/ZEN_Renderer2D.h"
 
 #endif // !ZEN_H
