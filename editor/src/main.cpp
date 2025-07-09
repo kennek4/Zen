50b82e7341169f77b25e644769ca91052333135c
kennek4 <kentabanay@gmail.com>
2025-07-08 21:27:01 -0600

    test: Called shader creation

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -1,5 +1,4 @@
 #include "zen.h"
-#include <iostream>
 #include <memory>
 
 int main(int argc, char *argv[]) {
@@ -10,21 +9,9 @@ int main(int argc, char *argv[]) {
   winMetaData.flags = SDL_WINDOW_BORDERLESS | SDL_WINDOW_OPENGL;
 
   ZEN_Engine *engine = new ZEN_Engine(&winMetaData, ZEN_RENDERER_OPENGL);
-  std::cout << "Before Loading..." << std::endl;
 
-  std::shared_ptr<ZEN_Texture2D> snoopy1 =
-      ZEN_ResourceManager::getInstance().loadAlphaTexture(
-          "../data/snoopy1000x1000.jpg", "Snoopy", true);
-
-  std::shared_ptr<ZEN_Texture2D> snoopy2 =
-      ZEN_ResourceManager::getInstance().loadAlphaTexture(
-          "../data/snoopy1000x1000.jpg", "Snoopy1", true);
-
-  std::cout << "After Loading..." << std::endl;
-
-  std::cout << "Snoopy 1 glID: " << snoopy1->getId() << std::endl;
-  std::cout << "Snoopy 2 glID: " << snoopy2->getId() << std::endl;
-
-  std::cout << "After cout" << std::endl;
+  std::shared_ptr<ZEN_GLShader> shader =
+      ZEN_ResourceManager::getInstance().loadShader("./data/basic.vert",
+                                                    "./data/basic.frag");
   delete engine;
 };
