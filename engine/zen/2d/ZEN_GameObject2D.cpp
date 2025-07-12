9f4377ffc0f3553ac20fc55794208ff14545c449
kennek4 <kentabanay@gmail.com>
2025-07-11 20:43:30 -0600

    feat: Redefined definition and impl. of class

--- engine/zen/2d/ZEN_GameObject2D.cpp
+++ engine/zen/2d/ZEN_GameObject2D.cpp
@@ -1,57 +1,25 @@
 #include <zen/2d/ZEN_GameObject2D.h>
+#include <zen/resource/ZEN_ResourceManager.h>
+
+ZEN_GameObject2D::ZEN_GameObject2D(std::string objName, ZEN::Sprite objSprite) {
+  name = objName;
+  sprite = objSprite;
+};
 
 ZEN_GameObject2D::~ZEN_GameObject2D() {
 
 };
 
 void ZEN_GameObject2D::translate(float xOffset, float yOffset) {
-  m_transform.position.x += xOffset;
-  m_transform.position.y += yOffset;
+  position.x += xOffset;
+  position.y += yOffset;
 };
 
 void ZEN_GameObject2D::scale(float xScalar, float yScalar) {
-  m_transform.size.x *= xScalar;
-  m_transform.size.y *= yScalar;
+  size.x *= xScalar;
+  size.y *= yScalar;
 };
 
 void ZEN_GameObject2D::rotate(float rotationOffset) {
-  m_transform.rotation += rotationOffset;
+  rotation += rotationOffset;
 };
-
-void ZEN_GameObject2D::setPosition(float x, float y) {
-  m_transform.position.x = x;
-  m_transform.position.y = y;
-};
-
-void ZEN_GameObject2D::setPosition(glm::vec2 position) {
-  m_transform.position = position;
-};
-
-void ZEN_GameObject2D::setSize(float width, float height) {
-  m_transform.size.x = width;
-  m_transform.size.y = height;
-};
-
-void ZEN_GameObject2D::setSize(glm::vec2 size) { m_transform.size = size; };
-
-void ZEN_GameObject2D::setRotation(float rotation) {
-  m_transform.rotation = rotation;
-};
-
-void ZEN_GameObject2D::setTransform(ZEN::Transform2D transform) {
-  m_transform = transform;
-};
-
-glm::vec2 const &ZEN_GameObject2D::getPosition() {
-  return m_transform.position;
-};
-
-glm::vec2 const &ZEN_GameObject2D::getSize() { return m_transform.size; };
-
-float const &ZEN_GameObject2D::getRotation() { return m_transform.rotation; };
-
-ZEN::Transform2D const &ZEN_GameObject2D::getTransform() {
-  return m_transform;
-};
-
-ZEN::Sprite const &ZEN_GameObject2D::getSprite() { return m_sprite; };
