580ab7a8975ffeff22d8494ed106cc9404217839
kris-perezz <kris.ilijay.perez@gmail.com>
2025-07-20 05:45:46 -0600

    chore: Reformatted previous stdouts into the proper ZEN logging  implementation and removed temporary spdlog example code.

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -5,8 +5,6 @@
 #include <iostream>
 #include <memory>
 
-
-
 int main(int argc, char *argv[]) {
 
   using ZENLOG = ZEN_LoggerManager; //ZENLOG is an alias for ZEN_LogggerManager
@@ -19,7 +17,7 @@ int main(int argc, char *argv[]) {
   winMetaData.flags = SDL_WINDOW_BORDERLESS | SDL_WINDOW_OPENGL;
 
   ZEN_Engine *engine = new ZEN_Engine(&winMetaData, ZEN_RENDERER_OPENGL);
-  ZENLOG::info("Before Loading...");
+  ZENLOG::trace("Before Loading...");
 
   std::shared_ptr<ZEN_Texture2D> snoopy1 =
       ZEN_ResourceManager::getInstance().loadAlphaTexture(
@@ -29,40 +27,12 @@ int main(int argc, char *argv[]) {
       ZEN_ResourceManager::getInstance().loadAlphaTexture(
           "../../data/snoopy1000x1000.jpg", "Snoopy", true);
 
-  std::cout << "After Loading..." << std::endl;
+  ZENLOG::trace("After Loading...");
 
-  std::cout << "Snoopy 1 glID: " << snoopy1->getId() << std::endl;
-  std::cout << "Snoopy 2 glID: " << snoopy2->getId() << std::endl;
+  ZENLOG::info("Snoopy 1 glID: ",snoopy1->getId());
+  ZENLOG::info("Snoopy 2 glID: ",snoopy2->getId());
 
-  std::cout << "After cout" << std::endl;
-
-  spdlog::info("Welcome to spdlog!");
-  spdlog::error("Some error message with arg: {}", 1);
-  
-  spdlog::warn("Easy padding in numbers like {:08d}", 12);
-  spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
-  spdlog::info("Support for floats {:03.2f}", 1.23456);
-  spdlog::info("Positional args are {1} {0}..", "too", "supported");
-  spdlog::info("{:<30}", "left aligned");
-  
-  //spdlog::set_level(spdlog::level::debug); // Set global log level to debug
-  spdlog::debug("This message should be displayed..");    
-  
-  // change log pattern
-  //spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
-  
-  // Compile time log levels
-  // Note that this does not change the current log level, it will only
-  // remove (depending on SPDLOG_ACTIVE_LEVEL) the call on the release code.
-  //SPDLOG_TRACE("Some trace message with param {}", 42);
-  //SPDLOG_DEBUG("Some debug message");
-  
-  
-  
-  ZENLOG::info("hi");
-  ZENLOG::warn("Snoopy 1 glID: {}", snoopy1->getId());
-  ZENLOG::trace("CAN YOU SEE THIS");
-  ZENLOG::debug("CAN YOU SEE THIS");
+  ZENLOG::trace("After cout");
 
   delete engine;
 };
