1cc76c54bfdf7eefc5b3431fe3c6a1f07697fe66
kris-perezz <kris.ilijay.perez@gmail.com>
2025-07-20 05:41:34 -0600

    chore: removed C++ version checker. Also reminds me that I updated the cmake to have c++ 23 as the standard.

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -64,14 +64,5 @@ int main(int argc, char *argv[]) {
   ZENLOG::trace("CAN YOU SEE THIS");
   ZENLOG::debug("CAN YOU SEE THIS");
 
-  if (__cplusplus == 202302L) std::cout << "C++23";
-    else if (__cplusplus == 202002L) std::cout << "C++20";
-    else if (__cplusplus == 201703L) std::cout << "C++17";
-    else if (__cplusplus == 201402L) std::cout << "C++14";
-    else if (__cplusplus == 201103L) std::cout << "C++11";
-    else if (__cplusplus == 199711L) std::cout << "C++98";
-    else std::cout << "pre-standard C++." << __cplusplus;
-    std::cout << "\n";
-
   delete engine;
 };
