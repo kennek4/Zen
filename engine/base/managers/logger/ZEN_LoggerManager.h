332c2a8dfe13cb9cee54559771fa686db1b03477
kris-perezz <kris.ilijay.perez@gmail.com>
2025-07-22 16:56:06 -0600

    feat: changed all spdlog namespace logging to spdlog macro logging.

--- engine/base/managers/logger/ZEN_LoggerManager.h
+++ engine/base/managers/logger/ZEN_LoggerManager.h
@@ -14,33 +14,38 @@ class ZEN_LoggerManager {
 
     template <typename... Args>
     static void trace(fmt::format_string<Args...> fmt, Args&&... args) {
+      spdlog::trace(fmt, std::forward<Args>(args)...);
       SPDLOG_TRACE(fmt, std::forward<Args>(args)...);
     }
 
     template <typename... Args>
     static void debug(fmt::format_string<Args...> fmt, Args&&... args) {
-      spdlog::debug(fmt, std::forward<Args>(args)...);
+      //spdlog::debug(fmt, std::forward<Args>(args)...);
       SPDLOG_DEBUG(fmt, std::forward<Args>(args)...);
     }
 
     template <typename... Args>
     static void info(fmt::format_string<Args...> fmt, Args&&... args) {
-      spdlog::info(fmt, std::forward<Args>(args)...);
+      //spdlog::info(fmt, std::forward<Args>(args)...);
+      SPDLOG_INFO(fmt, std::forward<Args>(args)...);
     }
 
     template <typename... Args>
     static void warn(fmt::format_string<Args...> fmt, Args&&... args) {
-      spdlog::warn(fmt, std::forward<Args>(args)...);
+      //spdlog::warn(fmt, std::forward<Args>(args)...);
+      SPDLOG_WARN(fmt, std::forward<Args>(args)...);
     }
 
     template <typename... Args>
     static void error(fmt::format_string<Args...> fmt, Args&&... args) {
-      spdlog::error(fmt, std::forward<Args>(args)...);
+      //spdlog::error(fmt, std::forward<Args>(args)...);
+      SPDLOG_ERROR(fmt, std::forward<Args>(args)...);
     }
 
     template <typename... Args>
     static void critical(fmt::format_string<Args...> fmt, Args&&... args) {
-      spdlog::critical(fmt, std::forward<Args>(args)...);
+      //spdlog::critical(fmt, std::forward<Args>(args)...);
+      SPDLOG_CRITICAL(fmt, std::forward<Args>(args)...);
     }
 
 };
