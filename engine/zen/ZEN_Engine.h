be4c57d3cdfa72e32bb10d5535ab25bd3d6642fc
kennek4 <kentabanay@gmail.com>
2025-07-26 00:02:07 -0600

    feat!: Deleted unused files
    The way the project was planned has now changed. These files are now no longer needed for the future development of the
    project.

--- engine/zen/ZEN_Engine.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <glm/ext/matrix_clip_space.hpp>
-#include <zen/ZEN_Types.h>
-#include <zen/render/ZEN_Renderer2D.h>
-#include <zen/resource/ZEN_ResourceManager.h>
-#include <zen/window/ZEN_WindowManager.h>
-
-class ZEN_Engine {};
