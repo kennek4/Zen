5a79f249231dfbf2412825cb1d63be080c01d175
kennek4 <kentabanay@gmail.com>
2025-07-26 00:15:50 -0600

    chore: Moved zen.h inside zen dir.

---
+++ engine/zen/zen.h
@@ -0,0 +1,12 @@
+#ifndef _ZEN_H
+#define _ZEN_H
+
+// Zen
+#include "zen/core/ZEN_Application.h"
+#include "zen/core/ZEN_Core.h"
+
+// ---------------- Zen Entry Point -----------------
+#include "zen/core/ZEN_EntryPoint.h"
+// --------------------------------------------------
+
+#endif // _ZEN_H
