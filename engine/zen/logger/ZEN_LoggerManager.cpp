2b50c78d796d4511ff299cb4814130509d9c1aaf
kris-perezz <kris.ilijay.perez@gmail.com>
2025-07-27 07:58:27 -0600

    feat: Added basic global logging to v0.1.0 and improved previous implentation from the old repo by creating macros

--- engine/zen/logger/ZEN_LoggerManager.cpp
+++ engine/zen/logger/ZEN_LoggerManager.cpp
@@ -1,13 +1,13 @@
 #include <zen/logger/ZEN_LoggerManager.h>
 //
-// #include <spdlog/sinks/basic_file_sink.h>
-// #include <spdlog/sinks/stdout_color_sinks.h>
+ #include <spdlog/sinks/basic_file_sink.h>
+ #include <spdlog/sinks/stdout_color_sinks.h>
 //
-// void ZEN_LoggerManager::init() {
-//     auto console = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-//     console->set_pattern("[%Y-%m-%d %H:%M:%S] [%^%l%$] %v");
-//
-//     auto logger = std::make_shared<spdlog::logger>("ZEN_Logger", console);
-//     logger->set_level(spdlog::level::trace);
-//     spdlog::set_default_logger(logger);
-// }
+void ZEN_LoggerManager::init() {
+    auto console = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    console->set_pattern("[%Y-%m-%d %H:%M:%S] [%^%l%$] %v");
+
+    auto logger = std::make_shared<spdlog::logger>("ZEN_Logger", console);
+    logger->set_level(spdlog::level::trace);
+    spdlog::set_default_logger(logger);
+}
